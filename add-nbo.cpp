f9bb17fb5b14134eb406159630d8f2c7ceaf4e3e
sssurim <thdtnfla97@gmail.com>
2021-07-22 03:16:16 -0400

    Add add-nbo

--- add-nbo.cpp
+++ add-nbo.cpp
@@ -4,38 +4,26 @@
 #include <netinet/in.h>
 #include <stdlib.h>
 
-int main(int argc, char** argv[]){
+int main(int argc, char *argv[]){
 
     FILE *fp1;
     FILE *fp2;
-    int buf_t, buf_h;
-    uint32_t n;
-    uint32_t m;
+    uint32_t n, m;
 
     fp1 = fopen(argv[1],"rb");
-    fread(&buf_t, sizeof(int), 1, fp1);
+    fread(&n, sizeof(int), 1, fp1);
     fclose(fp1);
 
-    n = ntohl(buf_t);
-    //printf("%04x\n",n);
-    //printf("%d\n",n);
-
+    n = ntohl(n);
+    
     fp2 = fopen(argv[2],"rb");
-    fread(&buf_h, sizeof(int), 1, fp2);
-
+    fread(&m, sizeof(int), 1, fp2);
     fclose(fp2);
 
 
-    m = ntohl(buf_h);
-    printf("%04x\n",m);
-    printf("%d\n",m);
+    m = ntohl(m);
 
-    uint32_t sum = n+m;
-    //printf("%d\n",sum);
-    //printf("%04x\n",sum);
-	
-	uint32_t sum = n+m;
-    printf("%d(%04x) + %d(%04x) = %d(%04x)\n",n,n,m,m,sum,sum);
+    printf("%d(%04x) + %d(%04x) = %d(%04x)\n",n,n,m,m,n+m,n+m);
 
     return 0;
 }
